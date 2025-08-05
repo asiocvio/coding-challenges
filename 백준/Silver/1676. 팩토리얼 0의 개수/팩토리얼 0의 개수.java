52107b3d35d7efc6ef4526b1aa3dc065c3f4109f
asiocvio <149280771+asiocvio@users.noreply.github.com>
2025-08-05 09:49:10 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 108 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int factorial = Integer.parseInt(br.readLine());
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 1; i <= factorial; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        int count = 0;
+        char[] resultChar = result.toString().toCharArray();
+        for (int i = resultChar.length - 1; i >= 0; i--) {
+            if(resultChar[i] == '0') {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
