6655ec564fefc7a9d8f66c9c1f0b86e3a191625f
asiocvio <149280771+asiocvio@users.noreply.github.com>
2025-08-12 23:42:18 +0900

    [Silver III] Title: 소수 구하기, Time: 1760 ms, Memory: 2996 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1000001;
+bool isPrime[MAX];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int m, n;
+    cin >> m >> n;
+
+    for (int i = 2; i <= n; i++)
+    {
+        isPrime[i] = true;
+    }
+
+    for (int p = 2; p * p <= n; p++)
+    {
+        if (isPrime[p])
+        {
+            for (int i = p * p; i <= n; i += p)
+            {
+                isPrime[i] = false;
+            }
+        }
+    }
+
+    for (int num = m; num <= n; num++)
+    {
+        if (isPrime[num])
+        {
+            cout << num << endl;
+        }
+    }
+
+    return 0;
+}
